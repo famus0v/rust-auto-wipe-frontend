92a953826f539b79637760b90658d16c4b5b89a0
Daniil Kahovich <famusov88@gmail.com>
2025-01-09 17:57:23 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="fill-height">
+    <h2>Привет</h2>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/pages/auth/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="d-flex align-center justify-center">
+    <v-card width="400px" variant="tonal">
+      <v-card-title>Авторизация</v-card-title>
+
+      <v-card-text>
+        <v-text-field placeholder="Логин" variant="solo-filled"></v-text-field>
+        <v-text-field placeholder="Пароль" type="password" variant="solo-filled"></v-text-field>
+        <v-btn>Войти</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+
+</script>
+
